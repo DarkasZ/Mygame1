c7636301667ac5b65d0267faca4f1fc8f1432d21
ACER270364\Windows <bossoyu0186@gmail.com>
2021-08-26 11:56:47 +0700

    no message

---
+++ Assets/AutoRotater.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotater : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Vector3 r = new Vector3(0, 0.1f, 0);
+        GetComponent<Transform>().Rotate(r);
+
+    }
+}
